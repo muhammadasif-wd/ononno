92585437779a94f93cee7554471b72e0355ad063
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-03-02 21:27:55 +0600

    asif

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <h1 className="text-3xl font-bold underline">
+        my name is aisf!
+      </h1>
+
     </div>
   );
 }
